aab3cbb679151f9c6c8986b34909feb64362f6f4
siontama <siontama@naver.com>
2022-03-11 14:44:34 +0900

    cmd

--- cmd.c
+++ cmd.c
@@ -6,7 +6,7 @@
 /*   By: sikang <sikang@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 17:09:21 by sikang            #+#    #+#             */
-/*   Updated: 2022/03/10 17:23:37 by sikang           ###   ########.fr       */
+/*   Updated: 2022/03/10 17:56:41 by sikang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,38 +15,40 @@
 void	cmd(t_stack *stack_a, t_stack *stack_b, char *cmd)
 {
 	ft_putstr_fd(cmd, 1);
-	if (ft_strcmp(cmd, "sa") == 0)
+	ft_putchar_fd('\n', 1);
+	if (ft_strncmp(cmd, "sa", 2) == 0)
 		ft_swap(stack_a);
-	else if (ft_strcmp(cmd, "sb") == 0)
+	else if (ft_strncmp(cmd, "sb", 2) == 0)
 		ft_swap(stack_b);
-	else if (ft_strcmp(cmd, "pb") == 0)
+	else if (ft_strncmp(cmd, "pb", 2) == 0)
 		ft_push(stack_a, stack_b);
-	else if (ft_strcmp(cmd, "pa") == 0)
+	else if (ft_strncmp(cmd, "pa", 2) == 0)
 		ft_push(stack_b, stack_a);
-	else if (ft_strcmp(cmd, "ra") == 0)
+	else if (ft_strncmp(cmd, "ra", 2) == 0)
 		ft_rotate(stack_a);
-	else if (ft_strcmp(cmd, "rb") == 0)
+	else if (ft_strncmp(cmd, "rb", 2) == 0)
 		ft_rotate(stack_b);
-	else if (ft_strcmp(cmd, "rra") == 0)
+	else if (ft_strncmp(cmd, "rra", 3) == 0)
 		ft_rev_rotate(stack_a);
-	else if (ft_strcmp(cmd, "rrb") == 0)
+	else if (ft_strncmp(cmd, "rrb", 3) == 0)
 		ft_rev_rotate(stack_b);
 }
 
 void	cmd_d(t_stack *stack_a, t_stack *stack_b, char *cmd)
 {
 	ft_putstr_fd(cmd, 1);
-	if (ft_strcmp(cmd, "ss") == 0)
+	ft_putchar_fd('\n', 1);
+	if (ft_strncmp(cmd, "ss", 2) == 0)
 	{
 		ft_swap(stack_a);
 		ft_swap(stack_b);
 	}
-	else if (ft_strcmp(cmd, "rr") == 0)
+	else if (ft_strncmp(cmd, "rr", 2) == 0)
 	{
 		ft_rotate(stack_a);
 		ft_rotate(stack_b);
 	}
-	else if (ft_strcmp(cmd, "rrr") == 0)
+	else if (ft_strncmp(cmd, "rrr", 3) == 0)
 	{
 		ft_rev_rotate(stack_a);
 		ft_rev_rotate(stack_b);
